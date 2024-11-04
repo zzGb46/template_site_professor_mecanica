ce44ff72f4c380e5ff2fd70cdf5c057bcd552c4d
GABRIEL MATHEUS ALVES DO NASCIMENTO SOUSA <gabriel.mansousa@senacsp.edu.br>
2024-11-04 11:08:06 -0300

    template site mecanica para estudo

---
+++ app/views/template/comompage.php
@@ -0,0 +1,14 @@
+<div data-src="assets/img/about-page-title.jpg" class="ak-bg">
+        <div class="ak-height-100 ak-height-lg-100"></div>
+        <div class="ak-height-125 ak-height-lg-40"></div>
+        <div class="container">
+            <div class="common-page-title">
+                <h3 class="page-title">About Us</h3>
+                <div class="d-flex gap-2 align-items-center">
+                    <p>Home /</p>
+                    <a href="/about.html">About Us</a>
+                </div>
+            </div>
+        </div>
+        <div class="ak-height-100 ak-height-lg-100"></div>
+    </div>
